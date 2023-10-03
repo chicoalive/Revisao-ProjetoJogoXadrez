ec67422ee398e63430b8758a604251f6a68f0c58
chicoalive <chicoalive@gmail.com>
2023-10-03 09:40:31 -0300

    Criando a “tabuleiroExceção” e aplicando a programação defensiva.

--- src/jogoTabuleiro/Tabuleiro.java
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -8,6 +8,10 @@ public class Tabuleiro {
     private Pecas[][] pecasMatriz;
 
     public Tabuleiro(int linhas, int colunas) {
+        // Aplicando a programação defensiva
+        if (linhas < 1 || colunas < 1) {
+            throw new TabuleiroExecao("Erro ao criar o tabuleiro: deve haver pelo menos 1 linha e 1 coluna.");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
 // Completando manualmente. A matriz de peças será instanciada com peças na quantidade de linhas e colunas informadas
@@ -24,29 +28,61 @@ public class Tabuleiro {
         return colunas;
     }
 
+    /*
+    Uma vez iniciado o tabuleiro, não podemos deixar que modifiquem o número de linhas e colunas. 
     public void setLinhas(int linhas) {
         this.linhas = linhas;
     }
 
     public void setColunas(int colunas) {
         this.colunas = colunas;
-    }
-
+    } */
 // Métodos 
     public Pecas pecas(int linha, int coluna) {
+        // Aplicando a programação defensiva
+        if (!posicaoExistente(linha, coluna)) {
+            throw new TabuleiroExecao("Posição não existe no tabuleiro.");
+        }
         return pecasMatriz[linha][coluna];
     }
 
     public Pecas pecas(Posicao posicao) {
+        // Aplicando a programação defensiva
+        if (!posicaoExistente(posicao)) {
+            throw new TabuleiroExecao("Posição não existe no tabuleiro.");
+        }
         return pecasMatriz[posicao.getLinha()][posicao.getColuna()];
     }
 // Método para impressão das peças. O método vai na matriz da peça em x linha e x coluna e atribui a posição a peça que vira como argumento.
 
     public void lugarPecas(Pecas peca, Posicao posicao) {
+        // Testando se já tem peça na posição
+        if (haPecas(posicao)) {
+            throw new TabuleiroExecao("Já existe uma peça nessa posição: " + posicao);
+        }
         pecasMatriz[posicao.getLinha()][posicao.getColuna()] = peca;
         /* Preciso informar que a peça não está mais na posição nula. A posição da peça é acessada diretamente,
         pois na classe pecas colocamos o atribuito protected e como a classe tabuleiro é do mesmo pacote, eu consigo acessar livremente a posição dela. */
         peca.posicao = posicao;
     }
 
+// Verificando a existência de posições e peças.
+    // Método auxiliar para o posicaoExistente.
+    private boolean posicaoExistente(int linha, int coluna) {
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
+
+    public boolean posicaoExistente(Posicao posicao) {
+        return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+    }
+
+    // Verificar se na posição já existe uma peça. 
+    public boolean haPecas(Posicao posicao) {
+        // Aplicando a programação defensiva
+        if (!posicaoExistente(posicao)) {
+            throw new TabuleiroExecao("Posição não existe no tabuleiro.");
+        }
+        return pecas(posicao) != null;
+
+    }
 }
---
+++ src/jogoTabuleiro/TabuleiroExecao.java
@@ -0,0 +1,9 @@
+package jogoTabuleiro;
+
+public class TabuleiroExecao extends RuntimeException {
+
+    public TabuleiroExecao(String message) {
+        super(message);
+    }
+
+}
